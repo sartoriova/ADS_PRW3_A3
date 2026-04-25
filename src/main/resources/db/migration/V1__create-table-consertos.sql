eb4c251ead396f1969bac40d062d39202b852ea7
sartoriova <valeriasartorio3@gmail.com>
2026-04-24 23:38:36 -0300

    fix: replacing int with varchar

--- src/main/resources/db/migration/V1__create-table-consertos.sql
+++ src/main/resources/db/migration/V1__create-table-consertos.sql
@@ -3,10 +3,10 @@ create table consertos (
     data_entrada varchar(10) not null,
     data_saida varchar(10) not null,
     nome_mecanico varchar(40) not null,
-    anos_experiencia int not null,
+    anos_experiencia varchar(4) not null,
     marca varchar(40) not null,
     modelo varchar(40) not null,
-    ano int not null,
+    ano varchar(4) not null,
 
     primary key (id)
  );
--- src/main/resources/db/migration/V4__alter-table-consertos-tipo-ano-veiculo.sql
+++
@@ -1,3 +0,0 @@
--- precisei criar porque tinha criado o ano do veículo como int inicialmente
--- depois só que vi que precisaria validar com Pattern, ops :)
-alter table consertos alter column ano type varchar(4);
