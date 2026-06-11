1fcf26115a443b41f0adfec4ffbdc8f586126b34
sartoriova <valeriasartorio3@gmail.com>
2026-06-11 20:56:05 -0300

    add tratarErro400 and DadosErroValidacao record in TratadorDeErros class

--- src/main/java/br/edu/ifsp/prw3/prw3_2026_AP3/util/TratadorDeErros.java
+++ src/main/java/br/edu/ifsp/prw3/prw3_2026_AP3/util/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package br.edu.ifsp.prw3.prw3_2026_AP3.util;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,5 +11,18 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400( MethodArgumentNotValidException ex ) {
+        var erros = ex.getFieldErrors();
+        var lista = erros.stream().map(DadosErroValidacao::new).toList();
+        return ResponseEntity.badRequest().body(lista);
+    }
+
+    private record DadosErroValidacao(String campo, String msgErro) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
